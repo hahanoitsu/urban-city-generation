b6844d0f22da9ad9a26ad79836136cfbcf47050b
hahanoitsu <105093069+hahanoitsu@users.noreply.github.com>
2026-08-27 00:10:45 +0800

    Export connectivity audit

--- src/urban_analysis/__init__.py
+++ src/urban_analysis/__init__.py
@@ -1,9 +1,11 @@
 from .analyse import analyse_manifest
+from .connectivity import audit_manifests
 from .morphology import MORPHOLOGY_CONTROL_FEATURES, MORPHOLOGY_FEATURES, describe_tile
 
 __all__ = [
     "MORPHOLOGY_CONTROL_FEATURES",
     "MORPHOLOGY_FEATURES",
     "analyse_manifest",
+    "audit_manifests",
     "describe_tile",
 ]
